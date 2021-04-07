5d1c393d9857584ff66a73bd8da97459f9c4e771
Surajbeone <75008599+Surajbeone@users.noreply.github.com>
2021-04-08 03:23:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home';
-import Sign from './components/Sign';
 import { NavigationContainer } from '@react-navigation/native';
 import {StackView} from '@react-navigation/stack'
 import { createStackNavigator } from '@react-navigation/stack';
+
+
 import Login from "./components/Login";
 import IconRefer from './components/IconRefer';
-import Menu from './components/Menu';
-import Form1 from './components/Form1';
+import Parking from './components/Parking';
 import Payments from './components/Payments';
+import Home from './components/Home';
+import Sign from './components/Sign';
+import Menu from './components/Menu';
+import P from './components/P';
 
 const Stack = createStackNavigator();
 
@@ -18,45 +21,15 @@ export default function App() {
   return (
     <NavigationContainer style={styles.Navigator}>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}  options={{headerStyle: {
-            backgroundColor: '#343c4c',
-            borderColor:'white',
-            borderWidth:1,
-            borderTopLeftRadius:35,
-            borderTopRightRadius:35
-          }}}/>
-          <Stack.Screen name="IconRefer" component={IconRefer} />
-        <Stack.Screen name="Sign" component={Sign} options={{headerStyle: {
-            backgroundColor: '#343c4c',
-            borderColor:'white',
-            borderWidth:1,
-            borderTopLeftRadius:35,
-            borderTopRightRadius:35
-          }}}/>
-        <Stack.Screen name="Login" component={Login} options={{headerStyle: {
-            backgroundColor: '#343c4c',
-            borderColor:'white',
-            borderWidth:1,
-            borderTopLeftRadius:35,
-            borderTopRightRadius:35
-          
-          }}}/>
-          <Stack.Screen name="Menu" component={Menu} options={{headerStyle: {
-            backgroundColor: '#343c4c',
-            borderColor:'white',
-            borderWidth:1,
-            borderTopLeftRadius:35,
-            borderTopRightRadius:35
-          
-          }}}/>
-          <Stack.Screen name="Payments" component={Payments} options={{headerStyle: {
-            backgroundColor: '#343c4c',
-            borderColor:'white',
-            borderWidth:1,
-            borderTopLeftRadius:35,
-            borderTopRightRadius:35
-          
-          }}}/>
+        <Stack.Screen name="Home" component={Home}  options={{headerStyle: styles.Headerstyle}}/>
+        <Stack.Screen name="IconRefer" component={IconRefer} />
+        <Stack.Screen name="Sign" component={Sign} options={{headerStyle: styles.Headerstyle}}/>
+        <Stack.Screen name="Login" component={Login} options={{headerStyle: styles.Headerstyle }}/>
+        <Stack.Screen name="Payments" component={Payments} options={{headerStyle: styles.Headerstyle}}/>
+        <Stack.Screen name="Menu" component={Menu} options={{headerStyle: styles.Headerstyle}}/>
+        <Stack.Screen name="Parking" component={Parking} options={{headerStyle: styles.Headerstyle}} />
+        
+        <Stack.Screen name="P" component={P} options={{headerStyle: styles.Headerstyle}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -67,7 +40,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'green',
   },
-  HeaderBorder : {
-    
+  Headerstyle : {
+    backgroundColor: '#343c4c',
+    borderColor:'white',
+    borderWidth:1,
+    borderTopLeftRadius:35,
+    borderTopRightRadius:35
   }
 });
